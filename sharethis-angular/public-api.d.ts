d1ba3de706e4871b29911b3745bf05211ed49317
lucasflomuller <lucasflorianomuller@gmail.com>
2020-11-12 09:09:00 -0300

    Auto-generated commit

--- sharethis-angular/public-api.d.ts
+++
@@ -1,6 +0,0 @@
-export * from './lib/sharethis-angular.module';
-export * from './lib/sticky-share-buttons/sticky-share-buttons.component';
-export * from './lib/inline-share-buttons/inline-share-buttons.component';
-export * from './lib/inline-follow-buttons/inline-follow-buttons.component';
-export * from './lib/inline-reaction-buttons/inline-reaction-buttons.component';
-//# sourceMappingURL=public-api.d.ts.map
--- sharethis-angular/sharethis-angular.d.ts
+++
@@ -1,6 +0,0 @@
-/**
- * Generated bundle index. Do not edit.
- */
-/// <amd-module name="sharethis-angular" />
-export * from './public-api';
-//# sourceMappingURL=sharethis-angular.d.ts.map
