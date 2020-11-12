034e1ee0ee9c5280805aad6bede556aada3c0015
lucasflomuller <lucasflorianomuller@gmail.com>
2020-11-12 09:00:13 -0300

    Auto-generated commit

--- lib/load.d.ts
+++
@@ -1,8 +0,0 @@
-declare global {
-    interface Window {
-        onShareThisLoaded: () => void;
-        __sharethis__: any;
-    }
-}
-export default function (component: any, product: any): void;
-//# sourceMappingURL=load.d.ts.map
